96a29dee0474cb09da300372a147201867ee32ff
Maurizio Drocco <drocco@di.unito.it>
2017-01-22 14:55:08 +0100

    minor fixes

--- examples/stock-market/run_stock_tweets.sh
+++ examples/stock-market/run_stock_tweets.sh
@@ -4,10 +4,10 @@ N_TWEETS=1M
 PORT=4000
 
 STOCK_FILE=testdata/nasdaq_europe.txt
-OUT_FILE=out/stock_tweets_output.txt
+OUT_FILE=out/stock_tweets.out
 
 #launch the stream generator and pipe it to netcat
-testdata/generate_tweets_sock $STOCK_FILE $N_TWEETS $PORT &
+testdata/generate_tweets $STOCK_FILE $N_TWEETS | nc -l $PORT &
 
 #launch the streaming pipeline
 ./stock_tweets $STOCK_FILE localhost $PORT > $OUT_FILE
