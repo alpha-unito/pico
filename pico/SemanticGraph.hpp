c7dde46d832a9fc1acd153e22171ea3112d39f5d
Maurizio Drocco <drocco@di.unito.it>
2018-01-14 11:12:00 +0100

    sketched semantic graph

--- pico/SemanticGraph.hpp
+++ pico/SemanticGraph.hpp
@@ -30,10 +30,17 @@
 
 #include <Pipe.hpp>
 
-class SematicGraph {
+enum DAGNodeRole {
+	Processing, Merge
+};
+
+class SemanticGraph {
 public:
-	SematicGraph(const Pipe &p) {
-		//TODO build
+	SemanticGraph() {
+	}
+
+	SemanticGraph(const Pipe &p) {
+		graph = make_graph(p);
 	}
 
 	void print() {
@@ -43,16 +50,74 @@ public:
 	void dot(std::ofstream &of) {
 		//TODO
 	}
+
+private:
+	struct SemDAGNode {
+		Operator *op = nullptr;
+		DAGNodeRole role = DAGNodeRole::Processing;
+
+		/**
+		 * Returns an unique name for the semDAGNode object.
+		 */
+		std::string name() const {
+			std::string name;
+			std::ostringstream address;
+			address << (void const *) this;
+			name = "a" + address.str().erase(0, 2);
+			return name;
+		}
+
+		std::string name_short() const {
+			if (role == DAGNodeRole::Processing) {
+				assert(op);
+				return op->name_short();
+			}
+			assert(role == DAGNodeRole::Merge);
+			return "Merge";
+		}
+	};
+
+	std::map<SemDAGNode*, std::vector<SemDAGNode*>> graph;
+	SemDAGNode *lastdagnode = nullptr, *firstdagnode = nullptr;
+
+	SemanticGraph *make_graph(const Pipe &p) const {
+		auto *res = new SemanticGraph();
+
+		switch (p.term_node_type) {
+		case Pipe::EMPTY:
+			break;
+		case Pipe::OPERATOR:
+			assert(p.term_value.op);
+			//TODO - singleton graph
+			break;
+		case Pipe::TO:
+			//TODO
+			//for (auto p_ : p.children)
+			//	res->add_stage(make_ff_term(*p_));
+			break;
+		case Pipe::ITERATE:
+			assert(p.children.size() == 1);
+			//TODO
+			//res->add_stage(make_ff_term(*p.children[0]));
+			//res->wrap_around();
+			break;
+		case Pipe::MERGE:
+			//TODO
+			//res->add_stage(make_merge_farm(p));
+			break;
+		}
+		return res;
+	}
 };
 
 void print_semantic_graph(const Pipe &p) {
-	SematicGraph g(p);
+	SemanticGraph g(p);
 	g.print();
 }
 
 void print_dot_semantic_graph(const Pipe &p, std::string fname) {
 	std::ofstream of(fname);
-	SematicGraph g(p);
+	SemanticGraph g(p);
 	g.dot(of);
 }
 
