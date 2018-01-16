9126c1db23bf0e20dccf756f5989fbe69fffe6db
Maurizio Drocco <drocco@di.unito.it>
2018-01-16 09:50:02 +0100

    print executor info

--- examples/word-count/pico_wc.cpp
+++ examples/word-count/pico_wc.cpp
@@ -79,7 +79,7 @@ int main(int argc, char** argv) {
 	/* execute the pipeline */
 	p2.run();
 
-	/* print the semantic DAG and generate dot file */
+	/* print the semantic graph and generate dot file */
 	p2.print_DAG();
 	p2.to_dotfile("wordcount.dot");
 
