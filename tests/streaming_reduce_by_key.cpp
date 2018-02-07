c3d8660f6dd53228b5cdef3ee36056f016616481
Maurizio Drocco <drocco@di.unito.it>
2018-02-07 18:45:39 -0500

    fixed test streaming_reduce_by_key

--- tests/streaming_reduce_by_key.cpp
+++ tests/streaming_reduce_by_key.cpp
@@ -13,63 +13,87 @@
 
 typedef KeyValue<char, int> KV;
 
-
-
-/*
- * input pairs
- * (A, -2)
- * (A, 3)
- * (B, 40)
- * (E, 4)
- * (C, 0)
- * (A, 5)
- * (B, 2)
- * (B, -4)
- * (F, -2)
- * (D, -2)
- * (C, -5)
- * (D, 2)
- *
- */
-
 /*
  * simple reduce by key with windowing that sums pairs.
  */
 
-TEST_CASE("streaming reduce by key", "streaming reduce by key tag" ){
+TEST_CASE("streaming reduce by key", "streaming reduce by key tag" ) {
 
+	/*
+	 * - read-from-stdin emulated by streaming a file to a socket on port 4000
+	 * - output inspected by redirecting stdout to a file
+	 */
+	std::string input_file = "common/occurrences.txt";
+	std::string output_file = "output.txt";
+	constexpr unsigned wsize = 3;
+
+	/* redirect stdout to output file */
+	auto coutbuf = std::cout.rdbuf(); //save old buf
+	std::ofstream out(output_file);
+	std::cout.rdbuf(out.rdbuf()); //redirect
+
+	/* build the pipeline */
 	ReadFromSocket reader("localhost", 4000, '\n');
-	std::cout<<"dopo read"<<std::endl;
+
 	WriteToStdOut<KV> writer([&](KV in) {
-			return in.to_string();
+		return in.to_string();
 	});
 
-	/* compose the pipeline */
 	auto test_pipe = Pipe() //the empty pipeline
-	.add(reader)
-	.add(Map<std::string, KV>([](std::string line) {//creates the pairs
-		auto res = KV::from_string(line);
-		return res;
-	}))
-	.add(ReduceByKey<KV>([](int v1, int v2) {return v1+v2;}).window(2)) //window size = 2
+	.add(reader).add(Map<std::string, KV>([](std::string line) { //creates the pairs
+				auto res = KV::from_string(line);
+				return res;
+			})) //
+	.add(ReduceByKey<KV>([](int v1, int v2) {return v1+v2;}).window(wsize)) //
 	.add(writer);
 
-	std::string output_file = "output_streaming.txt";
-	std::ofstream out(output_file);
-	std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
-	std::cout.rdbuf(out.rdbuf()); //redirect std::cout to output_file
+	/* execute the pipeline */
 	test_pipe.run();
-	std::cout.rdbuf(coutbuf); //reset to standard output again
 
-	std::string input_file = "numbers_file.txt";
+	/* undo stdout redirection */
+	std::cout.rdbuf(coutbuf);
 
+	/* parse output into grouped char-int pairs */
+	std::unordered_map<char, std::vector<KV>> observed;
+	auto output_pairs_str = read_lines(output_file);
+	for (auto kv_str : output_pairs_str) {
+		auto kv = KV::from_string(kv_str);
+		observed[kv.Key()].push_back(kv);
+	}
 
-	std::vector<std::string> output_pairs_str = read_lines(output_file);
-	std::unordered_multiset<std::string> output_pairs(output_pairs_str.begin(), output_pairs_str.end());
+	/*
+	 * emulate by-key grouping and windowing reduce
+	 */
+	std::unordered_map<char, std::vector<KV>> expected;
+	auto input_pairs_str = read_lines(input_file);
 
-	std::unordered_multiset<std::string> expected_pairs {KV('A',1).to_string(), KV('A',5).to_string(),
-		KV('B', 42).to_string(), KV('B', -4).to_string(), KV('C', -5).to_string(), KV('D', 0).to_string(),
-		KV('E', 4).to_string(), KV('F', -2).to_string()};
+	/* make groups */
+	std::unordered_map<char, std::vector<KV>> groups;
+	for (auto kv_str : input_pairs_str) {
+		auto kv = KV::from_string(kv_str);
+		groups[kv.Key()].push_back(kv);
+	}
 
-	REQUIRE(expected_pairs == output_pairs);
+	/* reduce each group per-window */
+	for (auto kgroup : groups) {
+		auto k(kgroup.first);
+		auto group(kgroup.second);
+		auto it = group.begin();
+		for (; it + wsize <= group.end(); it += wsize) {
+			//full window
+			int wres = 0;
+			for (auto wit = it; wit < it + wsize; ++wit)
+				wres += wit->Value();
+			expected[k].push_back(KV(k, wres));
+		}
+		if (it < group.end()) {
+			//remaining partial window
+			int wres = 0;
+			for (;it < group.end(); ++it)
+				wres += it->Value();
+			expected[k].push_back(KV(k, wres));
+		}
+	}
+
+	REQUIRE(expected == observed);
 }
