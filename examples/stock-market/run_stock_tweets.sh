81b95c4cc90d5798f8cbb09c4a7cd577520ad59f
Alberto Riccardo Martinelli <alberto.riccardo.martinelli@gmail.com>
2018-02-25 16:06:09 +0100

    updated wordcount and stock-market examples

--- examples/stock-market/run_stock_tweets.sh
+++ examples/stock-market/run_stock_tweets.sh
@@ -5,10 +5,10 @@ N_TWEETS=1M
 PORT=4000
 
 STOCK_FILE=testdata/nasdaq_europe.txt
-OUT_FILE=out/stock_tweets.out
+OUT_FILE=stock_tweets.out
 
 #launch the stream generator and pipe it to netcat
 testdata/generate_tweets $STOCK_FILE $N_TWEETS | nc -l $PORT &
 
 #launch the streaming pipeline
-./stock_tweets -i $STOCK_FILE  -w $W -b $MB -s localhost -p $PORT > $OUT_FILE
+./stock_tweets -w $W -b $MB $STOCK_FILE localhost $PORT > $OUT_FILE
--- examples/stock-market/run_stream.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p streamres/times
-for i in  1 2 4 8 16 32 42
-do
-	echo "running  $i on input ${1}"  
-	cat $1 | nc -l 4000 &
-	./stock_pricing_stream -s localhost -p 4000 -w $i -b 512 > streamres/times/outNP.log$i 2> streamres/times/err.log$i   && grep PiCo -A 1 streamres/times/outNP.log$i | head -n 5 
-done
