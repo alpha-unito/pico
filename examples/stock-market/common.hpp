ad75a0b9e2d5261c5fffea8c922654139fcc9658
Maurizio Drocco <drocco@di.unito.it>
2018-03-09 17:31:11 +0100

    missing file

---
+++ examples/stock-market/common.hpp
@@ -0,0 +1,39 @@
+/*
+ This file is part of PiCo.
+ PiCo is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ PiCo is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with PiCo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * common.hpp
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: drocco
+ */
+
+#ifndef EXAMPLES_STOCK_MARKET_COMMON_HPP_
+#define EXAMPLES_STOCK_MARKET_COMMON_HPP_
+
+#include <pico/pico.hpp>
+using namespace pico;
+
+#include "defs.h"
+
+static ReduceByKey<StockAndPrice> SPReducer([] (StockPrice p1, StockPrice p2) {
+	return std::max(p1,p2);
+});
+
+static WriteToStdOut<StockAndPrice> SPWriter([](StockAndPrice kv) {
+	return kv.to_string();
+});
+
+
+
+#endif /* EXAMPLES_STOCK_MARKET_COMMON_HPP_ */
