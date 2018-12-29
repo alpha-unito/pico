7292714b1b532304a2edff24dc7c86c768191994
Alberto Riccardo Martinelli <alberto.riccardo.martinelli@gmail.com>
2018-12-29 16:49:49 +0100

    [#51] ctest and catch integration

--- tests/create_inputs_tests.sh
+++ tests/create_inputs_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #generate tests
 echo "> generating tests"
 make
@@ -18,12 +19,3 @@ if [ ! -f pairs_64.txt ]; then
 ./generate_pairs 64 > pairs_64.txt
 fi
 cd ..
-
-# run
-echo "> running"
-echo "batch tests"
-./batch_tests
-
-echo "streaming tests"
-cat testdata/pairs.txt | nc -l 4000 &
-./stream_tests
